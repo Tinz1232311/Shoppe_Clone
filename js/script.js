2d021c4fc7d89c6318cd1af218b8283109dc1a57
tinzdrazx1 <tinzdrazx@gmail.com>
2025-08-21 22:51:54 +0700

    Finish-F8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const heartBtns = document.querySelectorAll(".home-product-item__like");
 const categoryList = document.querySelectorAll(".category-item");
+const headerSortList = document.querySelectorAll(".header__sort-item");
 
 heartBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -14,5 +15,15 @@ categoryList.forEach((categoryItem) => {
       activeItem.classList.remove("category-item--active");
     }
     categoryItem.classList.add("category-item--active");
+  });
+});
+
+headerSortList.forEach((headerSortItem) => {
+  headerSortItem.addEventListener("click", () => {
+    const activeItem = document.querySelector(".header__sort-item--active");
+    if (activeItem) {
+      activeItem.classList.remove("header__sort-item--active");
+    }
+    headerSortItem.classList.add("header__sort-item--active");
   });
 });
